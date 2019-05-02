12dff21ba92a42930aec35221e7048a223d8ebe7
Noor <barsanur@mail.ru>
2019-05-02 15:44:41 +0200

    My first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const  Header =  () =>{
+    return <header className='header'>
+        <img src='https://www.freelogodesign.org/Content/img/logo-ex-7.png'></img>
+      </header>
+}
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img src="https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+        </div>
+        <div>
+            {/* <img src="https://static.independent.co.uk/s3fs-public/thumbnails/image/2017/09/12/11/naturo-monkey-selfie.jpg?w968h681" /> */}
+            ava + description
+    </div>
+        <div>
+            My Post
+      <div>
+                new Post
+      </div>
+            <div>
+                Post 1
+      </div>
+            <div>
+                Post 2
+      </div>
+        </div>
+    </div>
+}
+
+export default Profile
